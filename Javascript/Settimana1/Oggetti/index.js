e0e918b6397a5458f453966197ebb83247ce1946
Ludovica.Spinelli <Ludovica.Spinelli@assiconsult.com>
2024-09-25 10:46:01 +0200

    build: completed ex about object

--- Javascript/Settimana1/Oggetti/index.js
+++ Javascript/Settimana1/Oggetti/index.js
@@ -49,3 +49,60 @@ console.log(value);
         },
     }
 } */
+
+const student = {
+    name: "Gino",
+    age: 17,
+    grade: 65,
+    school: "Carlona",
+}
+
+console.log(Object.keys(student));
+console.log(Object.values(student));
+console.log(Object.entries(student));
+
+for (const key in student) {
+    console.log(`${key}:  ${student[key]}`);
+}
+
+const numbers = [1, 2, 3, 4, 5];
+
+numbers.forEach((number, index, array) => {
+    console.log(number * 2);
+})
+
+const squareNumbers = numbers.map((number, index, array) => {
+    return number * number;
+})
+
+console.log(squareNumbers);
+
+const students = [{name: "Billy", grade: 78,}, {name: "Sara", grade: 59,}, {name: "Tim", grade: 64,}, {name: "Pina", grade: 93,}, {name: "Alice", grade: 47,}];
+
+const passedStudents = students.filter((student) => {
+    return student.grade >= 60;
+})
+
+console.log(passedStudents);
+
+const firstBadStudent = students.find((student) => {
+    return student.grade < 60;
+})
+
+console.log(firstBadStudent);
+
+const expenses = [37.2, 59.5, 26.22, 10.65, 5.8, 49.5, 37, 24.59, 38, 51.5];
+
+const sumExpenses = expenses.reduce((accumulator, current) => {
+    return accumulator + current
+}, 0);
+
+console.log(sumExpenses);
+
+const words = ["banana", "apple", "cherry", "date"];
+
+words.sort((a, b) => {
+    return a.localeCompare(b);
+})
+
+console.log(words);
