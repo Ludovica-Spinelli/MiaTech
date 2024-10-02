5126edb88ff2798479b66159c327a749c1aa8a88
Ludovica.Spinelli <Ludovica.Spinelli@assiconsult.com>
2024-10-02 15:48:18 +0200

    build: completed ex about interval and try catch

--- Javascript/Settimana3/tryCatch/index.js
+++ Javascript/Settimana3/tryCatch/index.js
@@ -63,6 +63,66 @@ const saluto = () => {
     console.log("Ciao!");
 }
 
-setTimeout(saluto, 3000);
-const interval = setInterval(saluto, 2000);
-clearInterval(interval);
+/* setTimeout(saluto, 3000);
+
+const twoSeconds = setInterval(saluto, 2000);
+setTimeout(() => {
+    clearInterval(twoSeconds);
+}, 5000)
+
+const oneSecond = setInterval(saluto, 1000);
+setTimeout(() => {
+    clearInterval(oneSecond);
+}, 5000) */
+
+const divisione = (divisore, dividendo) => {
+    try {
+        if(dividendo == 0){
+            throw new Error("Non si può dividere un numero per 0");
+        } else {
+            return divisore / dividendo;
+        }
+        
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+console.log(divisione(10, 2));
+divisione(10, 0);
+
+const operazioneStringa = (str1, str2) => {
+    try {
+        if(!(typeof str1 == "string") && !(typeof str2 == "string")) {
+            throw new Error("I valori inseriti devono essere due stringhe");
+        } else if(isNaN(str1 * str2)) {
+            throw new Error("Le stringhe devono essere due numeri");
+        } else {
+            return str1 * str2;
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+operazioneStringa(5, 2);
+operazioneStringa("5", "ciao");
+console.log(operazioneStringa("5", "2")); 
+
+const operazioneRiuscita = (valore1, valore2) => {
+    try {
+        if(typeof valore1 == "number" && typeof valore2 == "number" && valore1 >= 10) {
+            return valore1 + valore2;
+        } else {
+            throw new Error("I valori devono essere dei numeri e il primo valore non può essere inferiore a 10")
+        }
+    } catch (error) {
+        console.log(error.message);
+    } finally {
+        console.log("Operazione completata");
+    }
+} 
+
+operazioneRiuscita("ciao", 7);
+operazioneRiuscita(5, 7);
+console.log(operazioneRiuscita(15, 7));
